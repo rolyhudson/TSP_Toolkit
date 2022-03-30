4e9dd6c985ab6e60e15352886c0df4b95a20b911
rolyh <roly.hudson@gmail.com>
2022-03-30 17:30:05 +0100

    Add sun points object

---
+++ TSP_oM/SunPoints.cs
@@ -0,0 +1,15 @@
+﻿using BH.oM.Base;
+using BH.oM.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BH.oM.TSP
+{
+    public class SunPoints : BHoMObject
+    {
+        public virtual List<Point> Points { get; set; } = new List<Point>();
+
+        public virtual double Latitude { get; set; } = 0;
+    }
+}
